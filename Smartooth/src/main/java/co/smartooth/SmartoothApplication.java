9df8d04bcd40c71485f208292cbddc5aa39e2352
dev <dev@DESKTOP-B6J7I2B>
2022-06-07 16:17:54 +0900

    SmartoothApplication.java 파일 수정

--- Smartooth/src/main/java/co/smartooth/SmartoothApplication.java
+++ Smartooth/src/main/java/co/smartooth/SmartoothApplication.java
@@ -2,23 +2,12 @@ package co.smartooth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"co.smartooth"})
-public class SmartoothApplication extends SpringBootServletInitializer{
+public class SmartoothApplication {
 
-	//	WAR 패키징 할 때 유의사항
-	// @SpringBootApplication에 SpringBootServletInitializer 상속 받아서 configure 오버라이드 해야함
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return builder.sources(SmartoothApplication.class);
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SmartoothApplication.class, args);
 	}
