077349feb05c748dc9b4cf705c079ac982ef4a79
dev <dev@DESKTOP-B6J7I2B>
2022-06-07 10:07:57 +0900

    Gmail 비밀번호 변경

--- Smartooth/src/main/java/co/smartooth/controller/UserController.java
+++ Smartooth/src/main/java/co/smartooth/controller/UserController.java
@@ -8,12 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import co.smartooth.service.DeviceService;
 import co.smartooth.service.MailAuthService;
 import co.smartooth.service.UserService;
@@ -35,7 +35,7 @@ import co.smartooth.vo.UserVO;
 *************************/
 
 
-@RestController
+@Controller
 public class UserController {
 	
 	@Autowired(required = false)
